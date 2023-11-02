3cc07305cce6c6c122346a9798fd1f4b96fb5eb7
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2023-11-02 11:56:09 +0330

    create shopping list and recipes feature

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[]=[
+    new Recipe('Salad', 'This salad is healthy', 'https://www.healthygreenkitchen.com/wp-content/uploads/2022/02/healthy-quinoa-chicken-salad-bowl.jpg'), new Recipe("Pasta", "delicious Italian food", "https://www.licious.in/blog/wp-content/uploads/2020/12/Penne-Alfredo.jpg")
+  ];
 }
