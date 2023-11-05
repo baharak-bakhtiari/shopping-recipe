9a930d45e962a47b824ff3553a81de37862b9cfd
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2023-11-05 17:58:49 +0330

    add navigation to header links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  loadedFeature='recipe';
+
+  onNavigate(featureName:string){
+    this.loadedFeature=featureName;
+  }
+
+
 }
