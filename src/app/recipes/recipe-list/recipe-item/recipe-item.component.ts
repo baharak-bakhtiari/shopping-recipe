348ccd401b721dfd9d1d15277d2faa09128ed702
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2023-11-06 11:05:43 +0330

    display recipe detail component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,5 +7,11 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
+
+  @Output() recipeSelected=new EventEmitter<void>();
   @Input('recipe-item') recipeItem!:Recipe;
+
+  onRecipeSelect(){
+    this.recipeSelected.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
   recipes: Recipe[]=[
     new Recipe('Salad', 'This salad is healthy', 'https://www.healthygreenkitchen.com/wp-content/uploads/2022/02/healthy-quinoa-chicken-salad-bowl.jpg'), new Recipe("Pasta", "delicious Italian food", "https://www.licious.in/blog/wp-content/uploads/2020/12/Penne-Alfredo.jpg")
   ];
+
+  recipeJustSelected(recipeElement:Recipe){
+    this.recipeWasSelected.emit(recipeElement);
+  }
+
 }
