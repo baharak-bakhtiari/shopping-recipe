7d8c66759137f8b94c0f14f218d25025ee5cb147
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2024-01-21 11:33:01 +0330

    fix manage data button

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,9 @@ import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
+
   ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 10),
