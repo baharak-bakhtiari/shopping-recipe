679958a6f4ef366ef058f99e85021cc631a4a33a
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2024-02-13 13:19:09 +0330

    store user data on database

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { catchError, Subject, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   kind: string;
@@ -8,40 +9,61 @@ export interface AuthResponseData {
   email: string;
   refreshToken: string;
   expiresIn: string;
+  localId: string;
   registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  constructor(private http: HttpClient) {
+  user = new Subject<User>();
 
-  }
+  constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCi8DYs0oiw2qcSUCqIYeWmGNRixFxXBtI',
-      {
-        email: email,
-        password: password,
-        returnSourceToken: true
-      }
-    ).pipe(
-      catchError(this.handleError)
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCi8DYs0oiw2qcSUCqIYeWmGNRixFxXBtI',
+        {
+          email: email,
+          password: password,
+          returnSourceToken: true
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCi8DYs0oiw2qcSUCqIYeWmGNRixFxXBtI',
-      {
-        email: email,
-        password: password,
-        returnSourceToken: true
-      }
-    ).pipe(
-      catchError(this.handleError)
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCi8DYs0oiw2qcSUCqIYeWmGNRixFxXBtI',
+        {
+          email: email,
+          password: password,
+          returnSourceToken: true
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -61,5 +83,16 @@ export class AuthService {
         break;
     }
     return throwError(errorMessage);
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 }
