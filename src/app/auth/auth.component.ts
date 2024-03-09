baf0b5ae2f200a8ec4a746cf756b0551c6269eb4
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2024-03-09 15:22:21 +0330

    close alert on click

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -22,6 +22,10 @@ export class AuthComponent {
     this.isLogin = !this.isLogin;
   }
 
+  handleCloseError() {
+    this.error = null;
+  }
+
   onSubmit(form: NgForm) {
 
     if (!form.valid) return;
